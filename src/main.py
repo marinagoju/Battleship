fbb93329f1ff46d0edb78734e0b0bf3db3bebb27
yporquenoahora <yporquenoahora@gmail.com>
2023-02-15 19:44:25 +0100

    metodos de feedback para disparos

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from utils import Jugador
+from src.const import barcos, barcosNum
+
+
+tamTablero = 10 # ambos tienen el mismo tamaöo de tablero
+j1 = Jugador(False, tamTablero, "Aída\n")
+ordena = Jugador(True, tamTablero, "PC\n")
+
+j1.initTablero()
+ordena.initTablero()
+for i, v in barcosNum.items():    
+    j1.colocarBarcos(i,v)
+    ordena.colocarBarcos(i,v)
+
+j1.mostrarTablero()
+ordena.mostrarTablero()
